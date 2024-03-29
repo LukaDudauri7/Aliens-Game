defdd9734faa5547a7f1ee44e3c58d6b11461936
LukaDudauri7 <lukadudauri01@gmail.com>
2024-03-29 11:16:42 +0400

    event listeners

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 class Player {
-    constructor(game){
+    constructor(game) {
         this.game = game;
         this.width = 100;
         this.height = 100;
-        this.x = 200;
-        this.y = 200;
+        this.x = this.game.width * 0.5 - this.width * 0.5;
+        this.y = this.game.height * 0.8 - this.height;
+        this.speed = -1;
     }
     draw(context) {
         context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update() {
+        this.x += this.speed;
     }
 }
 
@@ -24,18 +28,37 @@ class Game {
         this.canvas = canvas;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+        this.keys = [];
+        this.player = new Player(this);
+
+        //event listeners
+        window.addEventListener('keydown', e => {
+            if(this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+        });
+
+        window.addEventListener('keyup', e => {
+            const index = this.keys.indexOf(e.key); 
+            if(index > -1) this.keys.splice(index, 1);
+        });
     }
-    render() {
-        // console.log(this.width, this.height);
+    render(context) {
+        this.player.draw(context);
+        this.player.update();
     }
 }
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 600;
     canvas.height = 800;
 
     const game = new Game(canvas)
-   game.render();
+    game.render(ctx);
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
 })
